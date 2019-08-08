898f85716ac6283162ca9b1d5532972eea493b16
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-08-08 18:19:18 +0200

    "Focus" on sapper issue

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,14 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<script>
+  let autofocus = null;
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  $: if (autofocus !== null) {
+    autofocus.focus();
+  }
+</script>
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<button
+  bind:this={autofocus}
+  on:blur={event => console.log(event.target, 'blurred towards', event.relatedTarget)}
+  on:focus={event => console.log(event.target, 'focused from', event.relatedTarget)}>
+  focused
+</button>
